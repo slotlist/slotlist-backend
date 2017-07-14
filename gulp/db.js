06e27f7827fb445249d773d7a390d4570abb3021
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2017-07-14 21:24:00 +0200

    Added gulp database tasks Modified docker dev task to run migrations upon startup Changed all fines to unix line endings

--- gulp/db.js
+++ gulp/db.js
@@ -1,3 +1,29 @@
+/* Required polyfills setup (see src/polyfills.ts) */
+const cls = require('continuation-local-storage');
+const uuid = require('uuid');
+const processCLSNamespace = cls.createNamespace(`SL_${uuid.v4()}`);
+const Sequelize = require('sequelize');
+Sequelize.useCLS(processCLSNamespace);
+const bluebird = require('bluebird');
+const patchBluebird = require('cls-bluebird');
+patchBluebird(processCLSNamespace);
+global.Promise = bluebird;
+console.info(`Polyfills installed. CLS=${processCLSNamespace.name}`);
+/* End of required polyfills setup */
+
 const gulp = require('gulp');
 const gulpUtil = require('gulp-util');
 const __ = require('./__');
+const storage = require('../dist/src/shared/services/Storage').default;
+
+gulp.task('db:migrate', __('Migrate database up to latest state', function (callback) {
+    return storage.migrateUp().then(cb).catch(cb);
+}));
+
+gulp.task('db:migrate:up', __('Migrate database ONE STEP UP from its current state', function (callback) {
+    return storage.migrateUp(false).then(cb).catch(cb);
+}));
+
+gulp.task('db:migrate:down', __('Migrate database ONE STEP DOWN from its current state', function (callback) {
+    return storage.migrateDown().then(cb).catch(cb);
+}));
