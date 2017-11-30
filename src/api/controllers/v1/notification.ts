29380fd353c4e616eb6ed180ec073a3c7df4ddcf
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2017-11-30 10:42:48 +0100

    Re-added pagination for notification list if `includeSeen` is true
    Allows for proper pagination of full notification list, but still sends all unread notifications at once

--- src/api/controllers/v1/notification.ts
+++ src/api/controllers/v1/notification.ts
@@ -1,4 +1,5 @@
 import * as Hapi from 'hapi';
+import * as _ from 'lodash';
 import * as moment from 'moment';
 
 import { Notification } from '../../../shared/models/Notification';
@@ -18,23 +19,47 @@ export function getNotificationList(request: Hapi.Request, reply: Hapi.ReplyWith
             order: [['createdAt', 'DESC']]
         };
 
-        if (!request.query.includeSeen) {
+        if (request.query.includeSeen) {
+            queryOptions.limit = request.query.limit;
+            queryOptions.offset = request.query.offset;
+
+            const result = await Notification.findAndCountAll(queryOptions);
+
+            const notificationCount = result.rows.length;
+            const moreAvailable = (queryOptions.offset + notificationCount) < result.count;
+            const notificationList = await Promise.map(result.rows, async (notification: Notification) => {
+                return notification.toPublicObject();
+            });
+
+            await Promise.map(_.filter(result.rows, (n: Notification) => _.isNil(n.seenAt)), (notification: Notification) => {
+                return notification.update({ seenAt: moment.utc() });
+            });
+
+            return {
+                limit: queryOptions.limit,
+                offset: queryOptions.offset,
+                count: notificationCount,
+                total: result.count,
+                moreAvailable: moreAvailable,
+                notifications: notificationList
+            };
+        } else {
             queryOptions.where.seenAt = null;
+
+            const notifications = await Notification.findAll(queryOptions);
+
+            const publicNotifications = await Promise.map(notifications, async (notification: Notification) => {
+                return notification.toPublicObject();
+            });
+
+            await Promise.map(notifications, (notification: Notification) => {
+                return notification.update({ seenAt: moment.utc() });
+            });
+
+            return {
+                notifications: publicNotifications
+            };
         }
-
-        const notifications = await Notification.findAll(queryOptions);
-
-        const publicNotifications = await Promise.map(notifications, async (notification: Notification) => {
-            return notification.toPublicObject();
-        });
-
-        await Promise.map(notifications, (notification: Notification) => {
-            return notification.update({ seenAt: moment.utc() });
-        });
-
-        return {
-            notifications: publicNotifications
-        };
     })());
 }
 
--- src/api/routes/v1/notification.ts
+++ src/api/routes/v1/notification.ts
@@ -25,9 +25,9 @@ export const notifications = [
                 strategy: 'jwt',
                 mode: 'required'
             },
-            description: 'Returns a list of all notifications for the user',
-            notes: 'Returns a list of all notifications for the user, only including unseen ones by default. Since retrieving a notification will mark it as seen immediately, ' +
-            'no pagination is available for this endpoint. Regular user authentication is required to access this endpoint',
+            description: 'Returns a list of notifications for the user',
+            notes: 'Returns a list of notifications for the user, only including unseen ones by default. Since retrieving a notification will mark it as seen immediately, ' +
+            'no pagination is available unless the `includeUnseen` flag has been set. Regular user authentication is required to access this endpoint',
             tags: ['api', 'get', 'v1', 'notifications', 'list'],
             validate: {
                 options: {
@@ -37,13 +37,30 @@ export const notifications = [
                     authorization: Joi.string().min(1).required().description('`JWT <TOKEN>` used for authorization, required').example('JWT <TOKEN>')
                 }).unknown(true),
                 query: Joi.object().required().keys({
+                    limit: Joi.number().integer().positive().min(1).max(LIMITS.notificationList.max).default(LIMITS.notificationList.default).optional()
+                        .description('Limit for number of notifications to retrieve, defaults to 25 (used for pagination in combination with offset). Only effective if ' +
+                        '`includeSeen` has been set to `true`'),
+                    offset: Joi.number().integer().min(0).default(0).optional()
+                        .description('Number of notifications to skip before retrieving new ones from database, defaults to 0 (used for pagination in combination with limit). ' +
+                        'Only effective if `includeSeen` has been set to `true`'),
                     includeSeen: Joi.bool().default(false).optional().description('Toggles whether already seen notifications should also be included in the retrieved list')
                 })
             },
             response: {
                 schema: Joi.object().required().keys({
+                    limit: Joi.number().integer().positive().min(1).max(LIMITS.notificationList.max).optional()
+                        .description('Limit for number of notifications to retrieve, as provided via query. Only present if query with `includeSeen` was executed'),
+                    offset: Joi.number().integer().positive().allow(0).min(0).optional()
+                        .description('Number of notifications to skip before retrieving new ones from database, as provided via query. Only present if query with `includeSeen` ' +
+                        'was executed'),
+                    count: Joi.number().integer().positive().allow(0).min(0).max(LIMITS.notificationList.max).optional()
+                        .description('Actual number of notifications returned. Only present if query with `includeSeen` was executed'),
+                    total: Joi.number().integer().positive().allow(0).min(0).optional().description('Total number of notifications stored. Only present if query with ' +
+                        '`includeSeen` was executed'),
+                    moreAvailable: Joi.bool().optional().description('Indicates whether more notifications are available and can be retrieved using pagination. Only present if ' +
+                        'query with `includeSeen` was executed'),
                     notifications: Joi.array().items(schemas.notificationSchema.optional()).required().description('List of notifications retrieved')
-                }).label('GetNotificationListResponse').description('Response containing list of currently unseen notifications')
+                }).label('GetNotificationListResponse').description('Response containing list of notifications')
             },
             plugins: {
                 'hapi-swagger': {
